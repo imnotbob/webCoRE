907fdf764ea3b707312a1e555741878be6f0149a
Imnotbob <Imnot_bob@hotmail.com>
2024-02-24 14:06:36 -0500

    Update versions

--- smartapps/ady624/webcore-piston.src/webcore-piston.groovy
+++ smartapps/ady624/webcore-piston.src/webcore-piston.groovy
@@ -18,7 +18,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not see <http://www.gnu.org/licenses/>.
  *
- * Last update January 24, 2024 for Hubitat
+ * Last update February 24, 2024 for Hubitat
  */
 
 //file:noinspection GroovySillyAssignment
